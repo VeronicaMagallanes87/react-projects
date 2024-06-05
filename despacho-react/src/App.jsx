788ed7af9daf787a36a3669ece0cf86dda78a62c
Veronica Magallanes <vmagallanes964@gmail.com>
2024-06-05 07:52:18 -0400

    Agregue el archivo Component.jsx

--- despacho-react/src/App.jsx
+++ despacho-react/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Component from './assets/Component'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
       <div>
-       
+        <img src="/losgilgares.jpeg"class="photo"></img>
+        <Component />
       </div>
-      
     </>
+
   )
 }
 
---
+++ despacho-react/src/assets/Component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Component() {
+    return (
+        <div> 
+            <h2>Bienvenido a la Agropecuaria Los Gilgares</h2>
+            <h3>Mi Nombre es Victor Vizcaya y voy a ser tu vendedor</h3>          
+            <p>Indica por favor tu Nombre y Teléfono celular</p>
+            Nombre : <input type="text" name="" id="" /><br></br>
+            Teléfono : <input type="text" name="" id="" />
+            <p>Mas perdida no puedo estar </p>
+            <p>Aquí voy</p>
+        </div>
+    )
+}
